9ae3bb06ac7e32b5a535676d5e6c9526bd1b3789
Maxim <maxim.kokosha@yanpix.com>
2017-07-06 14:27:28 +0300

    Fx calendar

--- src/app/kalendar-ockovania/kalendar-ockovania.component.ts
+++ src/app/kalendar-ockovania/kalendar-ockovania.component.ts
@@ -7,40 +7,29 @@ import {FormControl} from "@angular/forms";
   styleUrls: ['kalendar-ockovania.component.scss']
 })
 export class KalendarOckovaniaComponent implements OnInit {
-  years: any = [];
-  fullyear: any;
-  year0: any;
-  year1: any;
-  year2: any;
-  year3: any;
-  year4: any;
-  selectedValue: number;
-  yeaControl: FormControl;
+  years: number[];
+  fullYear: number;
+  circleYear: number;
+  yearControl: FormControl;
 
   constructor() {
     this.years = [];
-    this.yeaControl = new FormControl('');
+    this.yearControl = new FormControl('');
   }
 
   ngOnInit() {
-    this.fullyear = new Date().getFullYear();
+    this.fullYear = new Date().getFullYear();
     var range = [];
 
 
     for (var i = 0; i <= 67; i++) {
-      range.unshift(this.fullyear - i);
+      range.unshift(this.fullYear - i);
     }
 
     this.years = range;
-    this.year0 = this.selectedValue;
-    this.year1 = this.selectedValue + 1;
-    this.year2 = this.selectedValue + 6;
-    this.year3 = this.selectedValue + 11;
-    this.year4 = this.selectedValue + 16;
-
   }
 
-  onSubmit(value):void {
-    console.log(value);
+  toNumber() {
+    this.circleYear = +this.yearControl.value;
   }
 }
