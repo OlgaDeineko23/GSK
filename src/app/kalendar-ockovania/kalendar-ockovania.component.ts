37260f6dd39d419be6d82a3b448bd245dded6b7b
OlgaDeineko23 <olga.deineko@yanpix.com>
2017-07-06 13:04:28 +0300

    kalendar

--- src/app/kalendar-ockovania/kalendar-ockovania.component.ts
+++ src/app/kalendar-ockovania/kalendar-ockovania.component.ts
@@ -9,6 +9,12 @@ import {FormControl} from "@angular/forms";
 export class KalendarOckovaniaComponent implements OnInit {
   years: any = [];
   fullyear: any;
+  year0: any;
+  year1: any;
+  year2: any;
+  year3: any;
+  year4: any;
+  selectedValue: number;
   yeaControl: FormControl;
 
   constructor() {
@@ -26,6 +32,15 @@ export class KalendarOckovaniaComponent implements OnInit {
     }
 
     this.years = range;
+    this.year0 = this.selectedValue;
+    this.year1 = this.selectedValue + 1;
+    this.year2 = this.selectedValue + 6;
+    this.year3 = this.selectedValue + 11;
+    this.year4 = this.selectedValue + 16;
 
+  }
+
+  onSubmit(value):void {
+    console.log(value);
   }
 }
